d0a297bc6fb6bfe46c02268a2a3a1add5b1a9244
Jean Fontena <diegofontena@gmail.com>
2021-05-08 12:11:19 -0300

    Player uses a local instance of fluidsynth

--- player.py
+++ player.py
@@ -3,15 +3,16 @@ from mingus.midi import fluidsynth
 
 class Player:
     def __init__(self):
-        self.fluidsynth = fluidsynth.init(
+        self.fluidsynth = fluidsynth
+        self.fluidsynth.init(
             "/home/jeandiego/dev/tcp/GeneralUser_GS_SoftSynth_v144.sf2", "alsa")
 
     def play_note(self, note):
-        fluidsynth.play_Note(note)
+        self.fluidsynth.play_Note(note)
 
     def change_instrument(self, general_midi_instrument_id):
-        fluidsynth.set_instrument(1, general_midi_instrument_id)
+        self.fluidsynth.set_instrument(1, general_midi_instrument_id)
     
     def set_volume(self, volume):
         # maximum volume is 127
-        fluidsynth.main_volume(1, volume)
+        self.fluidsynth.main_volume(1, volume)
