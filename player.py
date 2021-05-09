b7380587da77c8f787b8da621b92766db1f37c84
Jean Fontena <diegofontena@gmail.com>
2021-05-09 12:31:20 -0300

    Remove comment from player

--- player.py
+++ player.py
@@ -10,9 +10,11 @@ class Player:
     def play_note(self, note):
         self.fluidsynth.play_Note(note)
 
+    def play_track(self, track):
+        self.fluidsynth.play_Track(track)
+
     def change_instrument(self, general_midi_instrument_id):
         self.fluidsynth.set_instrument(1, general_midi_instrument_id)
     
     def set_volume(self, volume):
-        # maximum volume is 127
         self.fluidsynth.main_volume(1, volume)
