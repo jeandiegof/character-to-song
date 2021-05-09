5b21fce25c1b957ea41013748e7bb1fe30c1ee75
Jean Fontena <diegofontena@gmail.com>
2021-05-09 12:58:31 -0300

    Simplify SongPlayer

--- song_player.py
+++ song_player.py
@@ -2,18 +2,21 @@ from player import Player
 
 import time
 
+
 class SongPlayer(Player):
     SECONDS_IN_ONE_MINUTE = 60
 
     def __init__(self, bpm, generic_midi_sf2_path, audio_driver):
+        super().__init__(generic_midi_sf2_path, audio_driver)
         self._bpm = bpm
-        self._player = Player(generic_midi_sf2_path, audio_driver)
 
     def play(self, song):
         for (note, instrument) in song:
-            if note is None:
-                time.sleep(self.SECONDS_IN_ONE_MINUTE / self._bpm)
-            else:
-                self._player.set_instrument(instrument)
-                self._player.play_note(note)
-                time.sleep(self.SECONDS_IN_ONE_MINUTE / self._bpm)
+            if note is not None:
+                self.set_instrument(instrument)
+                self.play_note(note)
+
+            time.sleep(self.beat_duration())
+
+    def beat_duration(self):
+        return self.SECONDS_IN_ONE_MINUTE / self._bpm
