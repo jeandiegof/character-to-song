3b8f04f40110888ace5762252f2beb8627d76734
Jean Fontena <diegofontena@gmail.com>
2021-05-09 12:14:03 -0300

    Add CommandExecutor and MidiBuilder

---
+++ command_executor.py
@@ -0,0 +1,73 @@
+from command import Command
+from midi_builder import MidiBuilder
+from player import Player
+
+from mingus.containers import Note
+
+
+class CommandExecutor:
+    MAXIMUM_VOLUME = 127
+    MAXIMUM_VALID_INSTRUMENT = 128
+    MAXIMUM_OCTAVE = 7
+
+    DEFAULT_OCTAVE = 5
+    DEFAULT_INSTRUMENT = 1
+    DEFAULT_VOLUME = 50
+
+    def __init__(self):
+        self._current_octave = self.DEFAULT_OCTAVE
+        self._current_volume = self.DEFAULT_VOLUME
+        self._current_instrument = self.DEFAULT_INSTRUMENT
+        self._last_command = None
+
+        self._midi_builder = MidiBuilder()
+
+    def execute(self, command):
+        if command is Command.change_instrument:
+            self._execute_change_instrument(command.value.data)
+        elif command is Command.add_offset_to_current_instrument:
+            self._execute_add_offset_to_current_instrument(command.value.data)
+        elif command is Command.play_note:
+            self._execute_play_note(command.value.data)
+        elif command is Command.increase_an_octave:
+            self._execute_increase_an_octave()
+        elif command is Command.double_volume:
+            self._execute_double_volume()
+        elif command is Command.repeat_or_pause:
+            self._execute_repeat_or_pause()
+
+        self._last_command = command
+
+    def get_resulting_track(self):
+        return self._midi_builder.get_track()
+
+    def _execute_change_instrument(self, instrument):
+        self._change_instrument(instrument)
+
+    def _execute_add_offset_to_current_instrument(self, offset):
+        target_instrument = self._current_instrument + offset
+        new_instrument = target_instrument if target_instrument <= self.MAXIMUM_VALID_INSTRUMENT else self.DEFAULT_INSTRUMENT
+        self._change_instrument(new_instrument)
+
+    def _execute_play_note(self, note_str):
+        note = Note(note_str, self._current_octave,
+                    velocity=self._current_volume)
+        self._midi_builder.append_note(note)
+
+    def _execute_increase_an_octave(self):
+        target_octave = self._current_octave + 1
+        self._current_octave = target_octave if target_octave <= self.MAXIMUM_OCTAVE else self.DEFAULT_OCTAVE
+
+    def _execute_double_volume(self):
+        target_volume = self._current_volume * 2
+        self._current_volume = target_volume if target_volume <= self.MAXIMUM_VOLUME else self.DEFAULT_VOLUME
+
+    def _execute_repeat_or_pause(self):
+        if self._last_command is Command.play_note:
+            self._execute_play_note(self._last_command.value.data)
+        else:
+            self._midi_builder.append_rest()
+
+    def _change_instrument(self, new_instrument):
+        self._midi_builder.set_instrument(new_instrument)
+        self._current_instrument = new_instrument
---
+++ midi_builder.py
@@ -0,0 +1,34 @@
+from mingus.containers.instrument import MidiInstrument
+from mingus.containers import Bar, Track
+
+class MidiBuilder:
+    def __init__(self):
+        self._instrument = MidiInstrument()
+        self._track = Track(self._instrument)
+        self._bar = Bar()
+
+    def append_note(self, note):
+        if self._bar.is_full():
+            self._add_bar_to_track()
+        
+        self._bar.place_notes(note, 4)
+
+    def append_rest(self):
+        if self._bar.is_full():
+            self._add_bar_to_track()
+        
+        self._bar.place_rest(4)
+
+    def get_track(self):
+        return self._track + self._bar
+
+    def set_instrument(self, generic_midi_id):
+        self._instrument.midi_instr = generic_midi_id
+
+    def _add_bar_to_track(self):
+        self._track = self._track + self._bar
+        self._bar = Bar()
+
+    # Are we missing the last samples?
+    def save_midi_to_file(self, path):
+        pass
