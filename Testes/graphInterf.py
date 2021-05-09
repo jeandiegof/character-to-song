6060b09b43b325d83471b162b73a2f1b7537de5b
Enzo Bonacina <epbonacina@gmail.com>
2021-05-09 19:24:19 -0300

    Novos layouts prontos e alguns bugs estão corrigidos

--- Testes/graphInterf.py
+++ Testes/graphInterf.py
@@ -1,71 +1,101 @@
 import PySimpleGUI as gui
+import os.path
 
-
-class GraphInterf:
+class Layouts:
 
     def startWindow(self):
-        gui.theme('DarkGray5')
         layout = [
-            [gui.Text('Software de Tradução Texto-Musical', font = ("Arial",22))],
+            [gui.Text('Software de Tradução Texto-Musical', font = ("Arial",20))],
             [gui.Text('Selecione uma das opções abaixo:')],
             [gui.Button('Abrir um arquivo de texto'), gui.Button('Digitar um texto')]
         ]
-        return gui.Window('StartWindow', layout=layout, finalize=True)
-
-    def textWindow(self):
-        gui.theme('DarkGray5')
-        layout = [
-            [gui.Text('Digite um texto abaixo')],
-            [gui.Multiline(size=(60,30))],
-            [gui.Button('Voltar'), gui.Button('Tradução musical')]
-        ]
-        return gui.Window('TextWindow', layout=layout, finalize=True)
+        return layout
 
     def browserWindow(self):
-        gui.theme('DarkGray5')
         layout = [
             [gui.Text('Digite o diretório do arquivo')],
-            [gui.Input()],
+            [gui.Input(key = 'address')],
             [gui.Button('Voltar'), gui.Button('Avançar')]
         ]
-        return gui.Window('BrowserWindow', layout=layout, finalize=True)
+        return layout
 
+    def textWindow(self, input = ''): 
+        layout = [
+            [gui.Text('Digite ou complemente o texto abaixo')],
+            [gui.Multiline(input, size=(60,30), key = 'textInput')],
+            [gui.Button('Voltar'), gui.Button('Tradução musical')]
+        ]
+        return layout
+
+class WindowManager:
+
+    def createWindow(self, layout):
+        return gui.Window('Conversor Musical', layout = layout, finalize=True)
+
+    def swapActiveWindow(self, oldWindow, newWindow):
+        self.hideWindow(oldWindow)
+        self.unHideWindow(newWindow)
+
+    def hideWindow(self, window):
+        window.hide()
+
+    def unHideWindow(self, window):
+        window.un_hide()
+
+class GraphInterf:
+
+    def __init__(self):
+        pass
 
 #Testes temporários
-InterfObj = GraphInterf()
-startWindow = InterfObj.startWindow()
-browserWindow = None
-textWindow = None
+manager = WindowManager()
+layouts = Layouts()
+
+startWindow = manager.createWindow(layouts.startWindow())
+browserWindow, textWindow, textWindow2 = None, None, None
 
 while True:
 
     window, event, values = gui.read_all_windows()
-
     if event == gui.WIN_CLOSED:
         break
 
     elif window == startWindow:
         if event == 'Abrir um arquivo de texto':
-            browserWindow = InterfObj.browserWindow()
-            startWindow.hide()
+            browserWindow = manager.createWindow(layouts.browserWindow())
+            manager.hideWindow(startWindow)
+
         elif event == 'Digitar um texto':
-            textWindow = InterfObj.textWindow()
-            startWindow.hide()
+            textWindow = manager.createWindow(layouts.textWindow())
+            manager.hideWindow(startWindow)
 
     elif window == browserWindow:
         if event == 'Voltar':
-            browserWindow.hide()
-            startWindow.un_hide()
+            manager.swapActiveWindow(browserWindow, startWindow)
+            
         elif event == 'Avançar':
             #verifica a existência do diretório e do arquivo
-            #e adiciona o texto do arquivo ao 'multiline'
-            browserWindow.hide()
-            textWindow = InterfObj.textWindow()
+            if(os.path.isfile(values['address'])):
+                file = open(values['address'], 'r')
+                text = file.read()
+                textWindow2 = manager.createWindow(layouts.textWindow(text))
+                manager.hideWindow(browserWindow)
+            else:
+                textWindow2 = manager.createWindow(layouts.textWindow('Não foi possível abrir o arquivo.'))
+                manager.hideWindow(browserWindow)
 
     elif window == textWindow:
         if event == 'Voltar':
-            textWindow.hide()
-            startWindow.un_hide()
+            manager.swapActiveWindow(textWindow, startWindow)
+
         elif event == 'Tradução musical':
-            #envia o arquivo de texto para a parte do jean
-            textWindow.hide()
+            #chama a função do jean, enviando o texto como parâmetro
+            manager.hideWindow(textWindow)
+
+    elif window == textWindow2:
+        if event == 'Voltar':
+            manager.swapActiveWindow(textWindow2, startWindow)
+
+        elif event == 'Tradução musical':
+            #chama a função do jean, enviando o texto como parâmetro
+            manager.hideWindow(textWindow2)
