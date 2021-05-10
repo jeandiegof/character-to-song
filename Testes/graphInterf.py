2a15ebd358f858fae5e53ce1b4edf5148d267923
Enzo Bonacina <epbonacina@gmail.com>
2021-05-10 14:58:58 -0300

    A interface está funcionando e se comunicando!

--- Testes/graphInterf.py
+++ Testes/graphInterf.py
@@ -5,11 +5,11 @@ class Layouts:
 
         mainWindow = [
             [gui.Text('Musical Translation Software', font = ("Arial",20))],
-            [gui.Text('Open file:', size = (20,1)), gui.FileBrowse(file_types=(("Text Files", "*.txt"),), target = 'addressOutput', size = (21,1), )],
-            [gui.Text('', visible = False, key = 'addressOutput', enable_events = True)],
-            [gui.Text('User input:', size = (20,1)), gui.Multiline('', size=(22,5), key = 'textInput')],
+            [gui.Text('Open file:', size = (20,1)), gui.FileBrowse(file_types=(("Text Files", "*.txt"),), target = 'FileAddress', size = (21,1), )],
+            [gui.Input(visible = False, key = 'FileAddress', enable_events = True)],
+            [gui.Text('User input:', size = (20,1)), gui.Multiline(size=(22,5), key = 'textInput')],
             [gui.Text('Status:', size = (21,1)), gui.Text('Waiting', key = 'statusOutput')],
-            [gui.Button('Play', size = (20,1)), gui.Button('Quit', size = (20,1))]
+            [gui.Button('Play | Stop', size = (20,1)), gui.Button('Quit', size = (20,1))]
         ]
 
 class WindowManager:
@@ -30,6 +30,7 @@ class WindowManager:
 class GraphInterf:
 
     def __init__(self, callback):
+        self.status = 'waiting'
         self.manager = WindowManager()
         self.window = self.manager.createWindow(Layouts.mainWindow)
         self._callback = callback
@@ -38,26 +39,30 @@ class GraphInterf:
         while True:
             self.window, self.event, self.values = gui.read_all_windows()
 
-            print(self.event)
-
             if self.event == gui.WIN_CLOSED or self.event == 'Quit':
                 self._callback(self.event,self.values)
                 break
 
-            elif self.event == 'Browse':
-                if os.path.isfile(self.values['addressOutput']):
-                    file = open(self.values['addressOutput'], 'r')
+            elif self.event == 'FileAddress':
+                if os.path.isfile(self.values['FileAddress']):
+                    file = open(self.values['FileAddress'], 'r')
                     text = file.read()
-                    window.Element['textInput'].Update(text)
+                    self.window.Element('textInput').Update(text)
                     self._callback(self.event, self.values)
                 else:
-                    window.Element['textInput'].Update('Please, select a valid address.')
+                    self.window.Element('textInput').Update('Please, select a valid address.')
 
-            elif self.event == 'Play':
-                self._callback(self.event,self.values)
-                self.window.Element('statusOutput').Update('Playing')
+            elif self.event == 'Play | Stop':
+                    self._callback(self.event,self.values)
+                    if (self.status == 'waiting'):
+                        self.status = 'playing'
+                        self.window.Element('statusOutput').Update('Playing')
+                    else:
+                        self.status = 'waiting'
+                        self.window.Element('statusOutput').Update('Waiting')
         self.window.Close()
 
+#TESTES
 def printzin(event, values):
     print('oi')
 
